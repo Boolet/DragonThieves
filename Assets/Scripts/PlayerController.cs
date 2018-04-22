02fb3635c7d3ec9db26d9f123d23a6db4323e4e6
Anthony Ha <haa4@uci.edu>
2018-04-22 15:37:00 -0700

    Basic Networking and Networked Player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    public float moveSpeed = 7.5f;
+
+    public Material localPlayerMat;
+    private void Start()
+    {
+        GetComponent<Rigidbody>().freezeRotation = true;
+    }
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().material = localPlayerMat;
+    }
+
+    void Update () {
+        if (!isLocalPlayer)
+            return;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(x, 0, z);
+	}
+}

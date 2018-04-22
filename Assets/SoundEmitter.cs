a965c649aeca73dfa35328f4ad32f4b4074417ae
Avi Miller <TrebecS@Gmail.com>
2018-04-22 14:22:38 -0700

    Working on Sound System

---
+++ Assets/SoundEmitter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEmitter : MonoBehaviour {
+
+	public static List<SoundEmitter> emitters = new List<SoundEmitter>();
+
+	[SerializeField] float speedFactor = 1f;
+	[SerializeField] float weightFactor = 1f;
+
+
+
+	void OnEnable(){
+		emitters.Add(this);
+	}
+
+	void OnDisable(){
+		emitters.Remove(this);
+	}
+
+	public float GetCurrentVolume(){
+
+	}
+
+	float SpeedComponent(Vector3 velocity){
+		return velocity.magnitude * speedFactor;
+	}
+
+	float WeightComponent(float weight){
+		return weight * weightFactor;
+	}
+}

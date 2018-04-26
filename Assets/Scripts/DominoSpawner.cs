0e7098de8ea878b90b81257e0c4d4afb599ce567
Avi Miller <TrebecS@Gmail.com>
2018-04-26 15:39:40 -0700

    Domino Spawning is okay

--- Assets/Scripts/DominoSpawner.cs
+++ Assets/Scripts/DominoSpawner.cs
@@ -3,31 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+//goes on player parent
 public class DominoSpawner : NetworkBehaviour {
 
 	[SerializeField] Camera cameraObject;
 	[SerializeField] DominoGravity dominoPrefab;
 	[SerializeField] GameObject dominoGhost;
+	[SerializeField] Material badGhostMaterial;
 	[SerializeField] Vector3 gravityDirection = Vector3.down;
 	[SerializeField] float surfaceTolerance = 2f;
 	[SerializeField] LayerMask spawnTargets;
 	[SerializeField] LayerMask dominoTargets;
 	[SerializeField] float rotationSensitivity = 2f;
+	[SerializeField] float placeDistance = 10f;
 
 	float currentRotationAngle = 0f;
 	GameObject ghostInstance;
+	OverlapDetector detector;
+	MeshRenderer ghostMesh;
+	Material goodGhostMaterial;
 	Vector3 targetPoint;
 	Vector3 targetNormal;
-	DominoSpawnBehavior currentMode = DominoSpawnBehavior.None;
+	DominoSpawnBehavior currentMode = DominoSpawnBehavior.Hover;
 
 	enum DominoSpawnBehavior{
-		None, Spawn, Delete
+		None, Spawn, Delete, Hover
 	}
 
 	void Start(){
+		CmdSpawnGhost();
+	}
+
+	[Command]
+	void CmdSpawnGhost(){
 		ghostInstance = Instantiate(dominoGhost);
 		NetworkServer.Spawn(ghostInstance);
-		ghostInstance.SetActive(false);
+		ghostMesh = ghostInstance.GetComponentInChildren<MeshRenderer>();
+		goodGhostMaterial = ghostMesh.material;
+		detector = ghostInstance.GetComponent<OverlapDetector>();
 	}
 
 	void Update () {
@@ -35,10 +48,12 @@ public class DominoSpawner : NetworkBehaviour {
 			return;
 		RotateDomino();
 		UpdateTarget();
+		PlaceGhost();
+		CheckOverlap();
+		SetGhostColor();
 		if (Input.GetMouseButtonUp(0)){
 			TrySpawnDomino();
 		}
-		PlaceGhost();
 	}
 
 	void RotateDomino(){
@@ -49,19 +64,21 @@ public class DominoSpawner : NetworkBehaviour {
 
 	bool AdaptiveRaycast(out RaycastHit hit, out DominoSpawnBehavior behavior){
 		behavior = DominoSpawnBehavior.None;
-		if(Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out hit, 200)){
-			if (( (1 << hit.collider.gameObject.layer) & spawnTargets) != 0){
+		if (Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out hit, placeDistance)){
+			if (((1 << hit.collider.gameObject.layer) & spawnTargets.value) != 0){
 				//hit a spawnable area
 				if (Vector3.Angle(gravityDirection * -1, hit.normal) <= surfaceTolerance){
 					behavior = DominoSpawnBehavior.Spawn;
 				}
-			} else if(((1 << hit.collider.gameObject.layer) & dominoTargets) != 0){
+			} else if (((1 << hit.collider.gameObject.layer) & dominoTargets.value) != 0){
 				//hit a domino
 				if (hit.collider.gameObject.GetComponentInParent<DominoGravity>().Gravity == gravityDirection.normalized){
 					behavior = DominoSpawnBehavior.Delete;
 				}
 			}
 			return true;
+		} else{
+			behavior = DominoSpawnBehavior.Hover;
 		}
 		return false;
 	}
@@ -74,13 +91,25 @@ public class DominoSpawner : NetworkBehaviour {
 		}
 	}
 
+	void SetGhostColor(){
+		if (currentMode == DominoSpawnBehavior.Spawn)
+			ghostMesh.material = goodGhostMaterial;
+		else
+			ghostMesh.material = badGhostMaterial;
+	}
+
 	void PlaceGhost(){
-		if (currentMode == DominoSpawnBehavior.Spawn){
+		ghostInstance.transform.rotation = Quaternion.AngleAxis(currentRotationAngle, gravityDirection);
+		if (currentMode != DominoSpawnBehavior.Hover){
 			ghostInstance.transform.position = targetPoint;
-			ghostInstance.transform.rotation = Quaternion.AngleAxis(currentRotationAngle, gravityDirection);
-			ghostInstance.SetActive(true);
 		} else{
-			ghostInstance.SetActive(false);
+			ghostInstance.transform.position = cameraObject.transform.position + cameraObject.transform.forward * placeDistance;
+		}
+	}
+
+	void CheckOverlap(){
+		if (detector.IsOverlapping()){
+			currentMode = DominoSpawnBehavior.None;
 		}
 	}
 
@@ -94,7 +123,7 @@ public class DominoSpawner : NetworkBehaviour {
 	void CmdSpawnDomino(Vector3 point){
 		Quaternion currentRotation = Quaternion.AngleAxis(currentRotationAngle, gravityDirection);
 		GameObject obj = Instantiate(dominoPrefab.gameObject, point, currentRotation);
-		dominoPrefab.Gravity = gravityDirection;
+		dominoPrefab.Gravity = gravityDirection;	//wrong
 		NetworkServer.Spawn(obj);
 	}
 }

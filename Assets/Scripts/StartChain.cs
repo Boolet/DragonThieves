6891f7fcda678bb18d4b6d57880514cce8c45e3b
Grant Hirahara <ghirahar@uci.edu>
2018-04-26 16:22:27 -0700

    Start Function
    Press the e button to make the first domino fall

---
+++ Assets/Scripts/StartChain.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartChain : MonoBehaviour {
+
+    public GameObject startDomino;
+    public float _x = 100;
+    public float _y = 0;
+    public float _z = 0;
+    public Rigidbody rb;
+    public bool canReset = false;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(canReset == false && Input.GetButtonDown("Start"))
+        {
+            //Debug.Log("Adding force");
+            canReset = true;
+            KnockDomino(_x, _y, _z);
+
+        }
+        /*if(canReset && Input.GetKeyDown("Start"))
+        {
+            canReset = false;
+        }*/
+	}
+    void KnockDomino(float x, float y, float z)
+    {
+        rb.AddForce(x, y, z);
+    }
+}

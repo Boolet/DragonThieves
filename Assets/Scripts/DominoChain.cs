a5c94572d765184ba2693be67b67265265965775
Anthony Ha <haa4@uci.edu>
2018-04-26 16:14:03 -0700

    Initial Domino Chain scripting

---
+++ Assets/Scripts/DominoChain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominoChain : MonoBehaviour {
+
+
+    public bool hitByDomino = false;
+    private void Awake()
+    {
+        hitByDomino = true;
+        GetComponent<Rigidbody>().AddForce(new Vector3(50, 0, 0));
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Domino" && collision.gameObject.GetComponent<DominoChain>().hitByDomino)
+        {
+            hitByDomino = true;
+            Debug.Log("Domino Chain");
+        }
+
+    }
+}

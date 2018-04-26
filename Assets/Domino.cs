ae6ffd529762924edf106a671539e0a4f6d2716e
Avi Miller <TrebecS@Gmail.com>
2018-04-26 16:37:17 -0700

    Serious edits to player

---
+++ Assets/Domino.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Domino : MonoBehaviour {
+
+	[SerializeField] Rigidbody dominoBody;
+
+	Quaternion spawnRotation;
+	Vector3 spawnPoint;
+
+	void Start(){
+		DominoTracker.RegisterDomino(this);
+		spawnPoint = dominoBody.transform.position;
+		spawnRotation = dominoBody.transform.rotation;
+	}
+
+	void OnDestroy(){
+		DominoTracker.UnregisterDomino(this);
+	}
+
+	public void Reset(){
+		dominoBody.velocity = Vector3.zero;
+		dominoBody.angularVelocity = Vector3.zero;
+		dominoBody.transform.position = spawnPoint;
+		dominoBody.transform.rotation = spawnRotation;
+	}
+}

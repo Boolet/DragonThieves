3d45ab37f8f582c97aba7f655fe610a6bd716484
Avi Miller <TrebecS@Gmail.com>
2018-04-26 11:47:05 -0700

    Improved domino spawner

---
+++ Assets/DominoGravity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominoGravity : MonoBehaviour {
+
+	Vector3 gravityOrientation = Vector3.zero;
+
+	Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponentInChildren<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (gravityOrientation == Vector3.zero)
+			return;
+		body.AddForce(gravityOrientation.normalized * Physics.gravity.magnitude * Time.fixedDeltaTime);
+	}
+
+	public Vector3 Gravity{
+		set{ gravityOrientation = value.normalized; }
+		get{ return gravityOrientation; }
+	}
+}
--- Assets/Scripts/DominoSpawner.cs
+++ Assets/Scripts/DominoSpawner.cs
@@ -6,47 +6,95 @@ using UnityEngine.Networking;
 public class DominoSpawner : NetworkBehaviour {
 
 	[SerializeField] Camera cameraObject;
-	[SerializeField] GameObject dominoPrefab;
+	[SerializeField] DominoGravity dominoPrefab;
+	[SerializeField] GameObject dominoGhost;
 	[SerializeField] Vector3 gravityDirection = Vector3.down;
+	[SerializeField] float surfaceTolerance = 2f;
 	[SerializeField] LayerMask spawnTargets;
 	[SerializeField] LayerMask dominoTargets;
+	[SerializeField] float rotationSensitivity = 2f;
 
-	Quaternion currentRotation = Quaternion.identity;
+	float currentRotationAngle = 0f;
+	GameObject ghostInstance;
+	Vector3 targetPoint;
+	Vector3 targetNormal;
+	DominoSpawnBehavior currentMode = DominoSpawnBehavior.None;
+
+	enum DominoSpawnBehavior{
+		None, Spawn, Delete
+	}
+
+	void Start(){
+		ghostInstance = Instantiate(dominoGhost);
+		NetworkServer.Spawn(ghostInstance);
+		ghostInstance.SetActive(false);
+	}
 
 	void Update () {
 		if (!isLocalPlayer)
 			return;
+		RotateDomino();
+		UpdateTarget();
 		if (Input.GetMouseButtonUp(0)){
 			TrySpawnDomino();
 		}
+		PlaceGhost();
 	}
 
-	void AdaptiveRaycast(){
-		RaycastHit hit;
+	void RotateDomino(){
+		float scroll = Input.mouseScrollDelta.y;
+		currentRotationAngle += scroll * rotationSensitivity;
+		currentRotationAngle = currentRotationAngle % 360;
+	}
+
+	bool AdaptiveRaycast(out RaycastHit hit, out DominoSpawnBehavior behavior){
+		behavior = DominoSpawnBehavior.None;
 		if(Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out hit, 200)){
-			if ((hit.collider.gameObject.layer & spawnTargets) != 0){
+			if (( (1 << hit.collider.gameObject.layer) & spawnTargets) != 0){
 				//hit a spawnable area
-				if (hit.normal == gravityDirection * -1){
-					CmdSpawnDomino(hit.point);
+				if (Vector3.Angle(gravityDirection * -1, hit.normal) <= surfaceTolerance){
+					behavior = DominoSpawnBehavior.Spawn;
 				}
-			} else if((hit.collider.gameObject.layer & dominoTargets) != 0){
+			} else if(((1 << hit.collider.gameObject.layer) & dominoTargets) != 0){
 				//hit a domino
+				if (hit.collider.gameObject.GetComponentInParent<DominoGravity>().Gravity == gravityDirection.normalized){
+					behavior = DominoSpawnBehavior.Delete;
+				}
 			}
+			return true;
+		}
+		return false;
+	}
+
+	void UpdateTarget(){
+		RaycastHit hit;
+		if (AdaptiveRaycast(out hit, out currentMode)){
+			targetPoint = hit.point;
+			targetNormal = hit.normal;
+		}
+	}
+
+	void PlaceGhost(){
+		if (currentMode == DominoSpawnBehavior.Spawn){
+			ghostInstance.transform.position = targetPoint;
+			ghostInstance.transform.rotation = Quaternion.AngleAxis(currentRotationAngle, gravityDirection);
+			ghostInstance.SetActive(true);
+		} else{
+			ghostInstance.SetActive(false);
 		}
 	}
 
 	void TrySpawnDomino(){
-		RaycastHit hit;
-		if(Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out hit, 200, spawnTargets.value)){
-			if (hit.normal == gravityDirection * -1){
-				CmdSpawnDomino(hit.point);
-			}
+		if (currentMode == DominoSpawnBehavior.Spawn){
+			CmdSpawnDomino(targetPoint);
 		}
 	}
 
 	[Command]
 	void CmdSpawnDomino(Vector3 point){
-		GameObject obj = Instantiate(dominoPrefab, point, currentRotation);
+		Quaternion currentRotation = Quaternion.AngleAxis(currentRotationAngle, gravityDirection);
+		GameObject obj = Instantiate(dominoPrefab.gameObject, point, currentRotation);
+		dominoPrefab.Gravity = gravityDirection;
 		NetworkServer.Spawn(obj);
 	}
 }

d3c46f8d4d55597599b8acb323ba5192c641d789
Anthony Ha <haa4@uci.edu>
2018-04-26 16:40:35 -0700

    cleanup

--- Assets/Scripts/DominoChain.cs
+++ Assets/Scripts/DominoChain.cs
@@ -6,19 +6,14 @@ public class DominoChain : MonoBehaviour {
 
 
     public bool hitByDomino = false;
-    private void Awake()
-    {
-        hitByDomino = true;
-        GetComponent<Rigidbody>().AddForce(new Vector3(50, 0, 0));
-        
-    }
+
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Domino" && collision.gameObject.GetComponent<DominoChain>().hitByDomino)
         {
             hitByDomino = true;
-            Debug.Log("Domino Chain");
+            //Debug.Log("Domino Chain");
         }
 
     }

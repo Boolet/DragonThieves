7ac5abf0f3f8d3b4accf2c30d01fe63b0324addc
Anthony Ha <haa4@uci.edu>
2018-04-29 22:05:00 -0700

    Barebones start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+	public void StartButton()
+    {
+        //Replace 1 with name/index of main game scene later
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
